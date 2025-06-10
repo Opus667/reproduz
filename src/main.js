63c75a9a98d33d02244d5555d9493285cc5239f9
Pepa <pepa@pepa.dev.br>
2025-06-10 17:43:26 -0300

    Apanhando do JS

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@
 import "@aarsteinmedia/dotlottie-player-light";
 
 document.addEventListener("DOMContentLoaded", function () {
+  let destaqueItem
   // Select all the .card elements
   const destaques = document.querySelectorAll(".dest");
   // Add event listeners to each card
@@ -19,19 +20,33 @@ document.addEventListener("DOMContentLoaded", function () {
       lottiePlayer.stop();
       lottiePlayer.play();
     });
-    destaque.addEventListener("click", function () {
+    destaque.addEventListener("click", function() {
+      console.log(destaqueItem + " e " +destaque.getAttribute("id"))
       destaques.forEach(function (destaque) {
         destaque.classList.remove("active");
       });
-      destaque.classList.add("active");
+      // destaqueItem == destaque.getAttribute("id") ? console.log(destaqueItem);
+      if (destaqueItem != destaque.getAttribute("id")){
+        destaque.classList.add("active");
+        destaqueItem = destaque.getAttribute("id");
+      } else {
+        destaqueItem = null;
+
+      };
+      });
+      /*     destaque.addEventListener("click", function () {
+      /*     destaque.addEventListener("click", function () {
+      console.log(destaque);
+      destaques.forEach(function (destaque) {
+        destaque.classList.remove("active");
+      });
+      !destaque.classList.contains("active") && destaque.classList.add("active");
     //   destaque.setTimeOut(console.log("teste"),2000);
-    setTimeout(() => {
-        console.log("teste");
-        // destaque.scrollIntoView({block: "center", behavior: "smooth" });
-        scrollY()
-    }, 500);
+      setTimeout(() => {
+        destaque.scrollIntoView({block: "center", behavior: "smooth" });
+      }, 2000);
     });
-    //   destaque.addEventListener('mouseleave', function() {
+ */    //   destaque.addEventListener('mouseleave', function() {
     //     // Find the dotlottie-player element within the current card
     //     const lottiePlayer = destaque.querySelector('dotlottie-player');
     //     // Stop the Lottie animation
