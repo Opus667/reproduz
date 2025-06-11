8972b9aade8cde88f096b1a7790449c090d8cccd
Pépa667 <pepa@pepa.dev.br>
2025-06-11 01:48:56 -0300

    Brigando com o Slider

--- src/main.js
+++ src/main.js
@@ -1,16 +1,18 @@
-// import { Application } from '@splinetool/runtime';
-// // import SimpleParallax from "simple-parallax-js/vanilla";
-
-// const canvas = document.getElementById('creative_3d');
-// const spline = new Application(canvas);
-// spline.load('/3d/creative.splinecode');
-
+import "itemslide"
 import "@aarsteinmedia/dotlottie-player-light";
 
 document.addEventListener("DOMContentLoaded", function () {
-  let destaqueItem
+  let destaqueItem;
   // Select all the .card elements
   const destaques = document.querySelectorAll(".dest");
+  if (window.matchMedia("(max-width: 600px)").matches) {
+    document.getElementById("dest_blank").remove();
+    destaques.forEach(function (destaque){
+      destaque.classList.remove("dest");
+      destaque.classList.add("destSwipe");
+    });
+    document.getElementById("destScroll").setAttribute("id","scrolling");
+  };
   // Add event listeners to each card
   destaques.forEach(function (destaque) {
     destaque.addEventListener("mouseenter", function () {
@@ -20,37 +22,41 @@ document.addEventListener("DOMContentLoaded", function () {
       lottiePlayer.stop();
       lottiePlayer.play();
     });
-    destaque.addEventListener("click", function() {
-      console.log(destaqueItem + " e " +destaque.getAttribute("id"))
+    destaque.addEventListener("click", function () {
       destaques.forEach(function (destaque) {
         destaque.classList.remove("active");
       });
-      // destaqueItem == destaque.getAttribute("id") ? console.log(destaqueItem);
-      if (destaqueItem != destaque.getAttribute("id")){
+      if (destaqueItem != destaque.getAttribute("id")) {
         destaque.classList.add("active");
         destaqueItem = destaque.getAttribute("id");
       } else {
         destaqueItem = null;
-
       };
-      });
-      /*     destaque.addEventListener("click", function () {
-      /*     destaque.addEventListener("click", function () {
-      console.log(destaque);
-      destaques.forEach(function (destaque) {
-        destaque.classList.remove("active");
-      });
-      !destaque.classList.contains("active") && destaque.classList.add("active");
-    //   destaque.setTimeOut(console.log("teste"),2000);
       setTimeout(() => {
         destaque.scrollIntoView({block: "center", behavior: "smooth" });
-      }, 2000);
+        console.log("teste2")
+      }, 750);
     });
- */    //   destaque.addEventListener('mouseleave', function() {
-    //     // Find the dotlottie-player element within the current card
-    //     const lottiePlayer = destaque.querySelector('dotlottie-player');
-    //     // Stop the Lottie animation
-    //     lottiePlayer.stop();
-    //   });
   });
+  const destaquesMain = document.getElementById("destaques");
+  destaquesMain.addEventListener("click", function (event) {
+    const parente = event.target.parentElement.getAttribute("id");
+    console.log(event.target.getAttribute("id"));
+    if (parente == "destaques" ||
+        parente == "dest_tittle"||
+        event.target.getAttribute("id") == "destaques"||
+        event.target.getAttribute("id") == "dest_blank" ) {
+      destaques.forEach(function (destaque) {
+      destaque.classList.remove("active");
+      destaqueItem = null;
+    });
+    };
+  });
+});
+
+var itemslide;
+
+window.addEventListener("load", () => {
+    var element = document.querySelector("#scrolling ul");
+    itemslide = new Itemslide(element, {});
 });
