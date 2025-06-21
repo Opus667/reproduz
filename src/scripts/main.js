d943294efd1c09644d4ae6e4a0e38a452f13b6fb
Pépa667 <pepa@pepa.dev.br>
2025-06-21 06:54:43 -0300

    Arrumando as Meta Tags

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,7 +10,6 @@ import PhotoSwipeLightbox from 'photoswipe/lightbox';
 import PhotoSwipeDynamicCaption from 'photoswipe-dynamic-caption-plugin';
 
 import '../scripts/updateGallery.js'; // só isso; não precisa exportar nada
-import '../scripts/resolveMetaImgs.js'; // só isso; não precisa exportar nada
 
 import 'photoswipe/style.css';
 import 'photoswipe-dynamic-caption-plugin/photoswipe-dynamic-caption-plugin.css';
