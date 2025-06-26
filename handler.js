cc17d6362cea518fa2a3dc3355c3d8cc3dff3720
Pépa667 <pepa@pepa.dev.br>
2025-06-25 21:26:53 -0300

    Teste de XDomain

--- handler.js
+++ handler.js
@@ -1,4 +1,3 @@
-import { handleRequest } from 'my-framework';
 import staticFiles from '@static-manifest';
 
 export const handler = async (request, { next }) => {
@@ -8,15 +7,8 @@ export const handler = async (request, { next }) => {
 
   // If your framework generates client assets in a subdirectory, you can add these too
   if (staticFiles.includes(pathname) || pathname.startsWith('assets/')) {
-    return;
+    return next();
   }
 
-  // "handleRequest" is defined by your framework
-  try {
-    return await handleRequest(request);
-  } catch (err) {
-    return new Response(err.message || 'Internal Server Error', {
-      status: err.status || 500,
-    });
-  }
+  return new Response('Hello World!');
 };
